fcc3bf81a5b2f642f182e6934b41d960cc18abaa
Anuj0124Kumar <118424881+Anuj0124Kumar@users.noreply.github.com>
2022-12-04 13:53:11 +0530

    Add files via upload

---
+++ shape.js
@@ -0,0 +1,16 @@
+ class shape{
+    shape;
+    Area;
+    colorOfShape;
+    drawShape(shape){
+        this.shape=shape;
+    }
+     
+    calculateArea(area){
+        this.area=area;
+    }
+
+    color(colorOfShape){
+        this.colorOfShape=colorOfShape;
+    }
+ }
